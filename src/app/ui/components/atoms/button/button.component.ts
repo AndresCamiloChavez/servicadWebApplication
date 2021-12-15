2300a73c39312ae1a2bb876351033fbbcde41cb8
Ingeniero Santiago <hsalfonsoc@rtisas.com>
2021-12-15 16:02:50 -0500

    Testing relation between components from other module

--- src/app/ui/components/atoms/button/button.component.ts
+++ src/app/ui/components/atoms/button/button.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-button',
+  selector: 'button-srv',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent implements OnInit {
+
+  @Input() titleButton = "";
 
   constructor() { }
 
