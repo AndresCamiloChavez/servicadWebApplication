3a176c0c4755d6c57e6b1a99884def4c1efbcfa3
Ingeniero Santiago <hsalfonsoc@rtisas.com>
2021-12-15 16:25:49 -0500

    Add components material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { PagesModule } from './pages/pages.module';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {PagesModule} from './pages/pages.module';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {UiModule} from "./ui/ui.module";
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -13,9 +14,12 @@ import {UiModule} from "./ui/ui.module";
   imports: [
     BrowserModule,
     PagesModule,
-    UiModule
+    UiModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
